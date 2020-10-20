774135a149446a3f5e9b9965d047bc7161b171f2
glebklimakhin <klimakhiny@mail.ru>
2020-10-21 00:16:15 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Controller {
+    @FXML
+    private TextField text1;
+    @FXML
+    public void onClick1()
+    {
+        System.out.println("Hello ");
+
+    }
+    @FXML
+    public void onClick2()
+    {
+        System.out.println("Bye!");
+    }
+
+
+}
